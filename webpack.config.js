d7604dd21c2777a4768c02f6a2988d4b427228c3
RobertBostrom4 <Robertbostrom4@gmail.com>
2023-08-13 19:10:17 -0600

    Setup initial webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+
+    module: {
+        rules: [
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    }
+
 };
